416191e30f83202a863f68a2b57d569f94816d4c
Imane Dibt <dibtimane@gmail.com>
2025-11-14 19:14:42 +0100

    Configure admin for bookshelf model

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# On crée une classe pour personnaliser l'affichage
+class BookAdmin(admin.ModelAdmin):
+    # Tâche 2.1: Affiche ces champs dans la liste
+    list_display = ('title', 'author', 'publication_year')
+
+    # Tâche 2.2: Ajoute une barre de recherche pour ces champs
+    search_fields = ('title', 'author')
+
+    # Tâche 2.2: Ajoute un filtre sur le côté
+    list_filter = ('publication_year', 'author')
+
+# On enregistre le modèle Book AVEC sa classe de personnalisation
+admin.site.register(Book, BookAdmin)
