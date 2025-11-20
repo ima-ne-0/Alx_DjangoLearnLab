6507931bb75637b8cdd68a5f80452dcf71677419
Imane Dibt <dibtimane@gmail.com>
2025-11-20 23:15:25 +0100

    Implemented Token Authentication and permissions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 # Création du routeur
 router = DefaultRouter()
@@ -8,9 +9,9 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Route existante pour la liste simple
+    
     path('books/', BookList.as_view(), name='book-list'),
 
-    # Inclusion des URLs générées par le routeur pour le CRUD complet
-    path('', include(router.urls)),
+        path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
