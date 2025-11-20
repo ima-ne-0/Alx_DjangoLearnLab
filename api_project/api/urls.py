2d2f5c880576bdfac301caaae64fe87e73bc615e
Imane Dibt <dibtimane@gmail.com>
2025-11-20 23:02:48 +0100

    Implemented BookViewSet and Router for CRUD operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,16 @@
-# api/urls.py
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+# Création du routeur
+router = DefaultRouter()
+# On enregistre notre ViewSet
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Route existante pour la liste simple
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Inclusion des URLs générées par le routeur pour le CRUD complet
+    path('', include(router.urls)),
 ]
