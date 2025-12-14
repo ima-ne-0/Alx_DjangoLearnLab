1bb64ead3a5bab93df4800c25321b3a6cafab37d
Imane Dibt <dibtimane@gmail.com>
2025-12-14 20:49:01 +0100

    Up date Social Media API project with authentication

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
